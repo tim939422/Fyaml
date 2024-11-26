30f5239d80d0184186ec9247c8fdc66c7a30b6ba
Barry Baker <bbakernoaa@users.noreply.github.com>
2024-11-25 23:38:37 -0500

    Update yaml_types.F90

--- src/yaml_types.F90
+++ src/yaml_types.F90
@@ -6,10 +6,16 @@ module yaml_types
     type(yaml_node), pointer :: children => null()
     type(yaml_node), pointer :: next => null()
     logical :: is_sequence = .false.
+    logical :: is_null = .false.
+    logical :: is_boolean = .false.
+    logical :: is_integer = .false.
+    logical :: is_float = .false.
+    logical :: is_string = .true.
+    character(len=:), allocatable :: anchor
   end type yaml_node
 
   type :: yaml_document
     type(yaml_node), pointer :: root => null()
+    type(yaml_node), pointer :: anchors(:) => null()
   end type yaml_document
 end module yaml_types
-
