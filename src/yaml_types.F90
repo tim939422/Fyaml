2a757a5a92342d795ba8d6b8d4fb44f223e1e25f
Barry Baker <bbakernoaa@users.noreply.github.com>
2024-11-25 23:17:40 -0500

    Update yaml_types.F90

--- src/yaml_types.F90
+++ src/yaml_types.F90
@@ -1,1 +1,15 @@
+module yaml_types
+  implicit none
+  type :: yaml_node
+    character(len=:), allocatable :: key
+    character(len=:), allocatable :: value
+    type(yaml_node), pointer :: children => null()
+    type(yaml_node), pointer :: next => null()
+    logical :: is_sequence = .false.
+  end type yaml_node
+
+  type :: yaml_document
+    type(yaml_node), pointer :: root => null()
+  end type yaml_document
+end module yaml_types
 
